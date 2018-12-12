b68e79ce6016ea4a82ffab35c88fe9a8e5396475
김형석 <hyungseokkim@gimhyeongseog-ui-MacBookPro.local>
2018-12-12 16:53:55 +0900

    Updates

---
+++ precache-manifest.15a050a66c6927415e61d511059fbd36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "b1a2fae981d2030d8aa9",
+    "url": "/movie_app/static/js/main.b1a2fae9.chunk.js"
+  },
+  {
+    "revision": "392d6e18dd1fb16cb5e2",
+    "url": "/movie_app/static/js/1.392d6e18.chunk.js"
+  },
+  {
+    "revision": "b1a2fae981d2030d8aa9",
+    "url": "/movie_app/static/css/main.d56e62aa.chunk.css"
+  },
+  {
+    "revision": "759f635cf1aa350e9216d9bc46a22631",
+    "url": "/movie_app/index.html"
+  }
+];
